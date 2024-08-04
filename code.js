9ae4952ac7b4851a74423e3ef5717012aa5f8e4b
Mjdiazri <citruss2012@hotmail.es>
2024-08-03 19:13:39 -0500

    Se crea archivo js.js con solucion para boton encriptador, y se crea archivo css

--- code.js
+++ code.js
@@ -25,7 +25,7 @@ conversor(texto);
 console.log(lista);
 */
 
-let texto = "aeiou";
+let texto = "AeIou";
 let lista = [];
 const vocales = ["o","e","i","o","u"];
 let lista2 =[];
@@ -76,11 +76,11 @@ conversor(texto);
 //let lista2 = lista.toSpliced(lista[0],1,"ai")
 //console.log(lista2)
 
-const reg = /a/g;
-const reg1 = /e/g;
-const reg2 = /i/g;
-const reg3 = /o/g;
-const reg4 = /u/g ;
+/*const reg = /a/gi;
+const reg1 = /e/gi;
+const reg2 = /i/gi;
+const reg3 = /o/gi;
+const reg4 = /u/gi;
 
 let texto3 = texto.replaceAll(reg, "ai");
 let texto4 = texto.replaceAll(reg1, "enter");
@@ -89,3 +89,5 @@ let texto5 = texto.replaceAll(reg1, "imes");
 console.log(texto3),
 console.log(texto4),
 console.log(texto5);
+*/
+
---
+++ js.js
@@ -0,0 +1,34 @@
+let texto = "aeiouAEIOUZZZKLJFDNFALKVN";
+let ffilter = [];
+let sfilter = [];
+
+function encriptar(){
+    ffilter = texto.split('');
+    sfilter = ffilter.map((caracter) => {
+        if (caracter === "a" || caracter === "A"){
+            return "ai";
+        } else if (caracter === "e" || caracter === "E"){
+            return "enter";
+        } else if (caracter === "i" || caracter === "I"){
+            return "imer";
+        } else if (caracter === "o" || caracter === "O"){
+            return "ober";
+        } else if (caracter === "u" || caracter === "U"){
+            return "ufat";
+        } else {
+            return caracter;
+        }
+    });
+}
+
+
+
+encriptar()
+
+console.log(ffilter);
+console.log(sfilter);
+
+
+
+
+
