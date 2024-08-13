7596e39a8df64e5e4dea848fc16439a533cb19d9
Mjdiazri <citruss2012@hotmail.es>
2024-08-13 16:32:29 -0500

    Se genera nuevo boton con funcion de copiar

--- Intentos/main.js
+++ Intentos/main.js
@@ -18,7 +18,10 @@ click.addEventListener('click', encriptar);
 const clack = document.getElementById('botontwo');
 clack.addEventListener('click', desencriptar);
 
+const clock = document.getElementById('botonthree');
 
+const cluck = document.getElementById('botonfour');
+cluck.addEventListener('click',condicionesIniciales);
 
 
 //Funciones 
@@ -48,7 +51,11 @@ function encriptar(){
                 return caracter;
             }
         })
-        mostrarTextEncrip();
+
+        //mostrarTextEncrip();
+        let valor = sfilter.join("");
+        console.log(valor)
+        mostrarText(valor)
     } else if (validacion === false){
         document.getElementById('mensajealerta').style.display = 'block';
         document.getElementById('entrada').style.border = 'red, solid';
@@ -61,36 +68,56 @@ function encriptar(){
 
  //Funcion desencriptar texto
  function desencriptar(){
-    ftexto = texto.replaceAll('ai','a')
-                  .replaceAll('enter','e')
-                  .replaceAll('imer','i')
-                  .replaceAll('ober','o')
-                  .replaceAll('ufat','u')
-    mostrarTextDesencrip();   
-}
+    let validacion = (validarText(texto));
+    console.log(validacion)
+    if (validacion === true){
+        ftexto = texto.replaceAll('ai','a')
+                        .replaceAll('enter','e')
+                        .replaceAll('imer','i')
+                        .replaceAll('ober','o')
+                        .replaceAll('ufat','u')
+       // mostrarTextDesencrip(); 
+       mostrarText(ftexto)  
+    } else if (validacion === false){
+        document.getElementById('mensajealerta').style.display = 'block';
+        document.getElementById('entrada').style.border = 'red, solid';
+        document.getElementById('entrada').style.boxShadow = '3px 0px 30px red';
+        const entrada = document.getElementById('entrada');
+        entrada.addEventListener("keydown", condicionesIniciales);
+    }
+ }
 
 //Funcion mostrar texto desencriptado
-function mostrarTextDesencrip(){    
+/*function mostrarTextDesencrip(){    
         document.getElementById('image').style.display = 'none';
         document.getElementById('initialtitle').style.display = 'none';
         document.getElementById('initialp').style.display = 'none';
         document.getElementById('finalp').style.display = 'block';
-        document.getElementById('finalp').innerText = ftexto;
+        document.getElementById('finalp').innerText = (ftexto);
 
  }
-
+*/
+ //Funcion mostrar texto general
+ function mostrarText(valor){
+        document.getElementById('image').style.display = 'none';
+        document.getElementById('initialtitle').style.display = 'none';
+        document.getElementById('initialp').style.display = 'none';
+        document.getElementById('finalp').style.display = 'block';
+        document.getElementById('finalp').innerText = (valor);
+ }
 
  //Funcion mostrar texto encriptado
- function mostrarTextEncrip(){    
+ /*function mostrarTextEncrip(){    
         document.getElementById('image').style.display = 'none';
         document.getElementById('initialtitle').style.display = 'none';
         document.getElementById('initialp').style.display = 'none';
         document.getElementById('finalp').style.display = 'block';
-        document.getElementById('finalp').innerText = sfilter.join("");
+        document.getElementById('finalp').innerText = (sfilter.join(""));
  }
-     
+*/   
  //Funcion condiciones iniciales
  function condicionesIniciales(){
+    document.getElementById('entrada').value = '';
     document.getElementById('image').style.display = 'block';
     document.getElementById('initialtitle').style.display = 'block';
     document.getElementById('initialp').style.display = 'block';
@@ -98,6 +125,7 @@ function mostrarTextDesencrip(){
     document.getElementById('mensajealerta').style.display = 'none';
     document.getElementById('entrada').style.border = 'darkblue, solid';
     document.getElementById('entrada').style.boxShadow = '3px 0px 30px rgb(97, 156, 224)';
+    document.getElementById('botonthree').innerText = ("Copiar");
     
  }
 
@@ -107,3 +135,18 @@ function validarText(variable) {
        return regex.test(variable)
 }
 
+
+//Funcion copiar
+
+const copiartext = async () => {
+    try {
+        const textToCopy = document.getElementById('finalp').innerText
+        await navigator.clipboard.writeText(textToCopy)
+        console.log('Texto copiado')
+        document.getElementById('botonthree').innerText = ("Copiado")
+    } catch (error) {
+        console.log(error)        
+    }
+}    
+
+clock.addEventListener('click', copiartext);
